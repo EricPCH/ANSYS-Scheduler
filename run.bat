af77e25d6420d437d1176560d932e8c7fa925625
Lin, Ming Chih <mingchih.lin8@gmail.com>
2025-06-28 15:50:09 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -4,5 +4,3 @@ set "ANSYSEDT_PATH=C:\Program Files\ANSYS Inc\v251\AnsysEM\ansysedt"
 
 echo 執行 IronPython 腳本...
 "C:\Program Files\ANSYS Inc\v251\AnsysEM\common\IronPython\ipy64.exe" scheduler.py
-
-pause
